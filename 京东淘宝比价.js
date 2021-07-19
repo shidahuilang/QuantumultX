dc1d4be7b72bf4bc743efe201eb1c76f9be30868
shidahuilang <jmkongdeshuai@gmail.com>
2021-07-19 22:39:57 +0800

    ·

---
+++ 京东淘宝比价.js
@@ -0,0 +1,9 @@
+#[rewrite_local]
+#这个位置用于填写您自己找到的其他未被此懒人包收录的脚本（请回车另起一行）
+
+# 京东比价
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+
+# 淘宝比价
+^http://.+/amdc/mobileDispatch url script-request-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
